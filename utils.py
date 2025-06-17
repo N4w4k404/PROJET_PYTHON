d805a25789cbb1e6d505dd48c4d96c9c9197262a
n4w4k_ <133209963+N4w4k404@users.noreply.github.com>
2025-06-17 02:39:17 +0200

    feat: fetures ok

--- utils.py
+++ utils.py
@@ -13,15 +13,16 @@ import psutil
 import hashlib
 # Gestion des regex
 import re
+# Import de scapy pour l'analyse réseau
+from scapy.all import *
+
+# =================== Vérification des IPs =================== 
 
 def valid_ip(ip):
-
     # Regex de l'IPv4
     regex = r'^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$'
-
     # Regex de l'IPv6
     regex1 = r'^(?:(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$'
-
     # regex IPV4
     p = re.compile(regex)
     # Regex IPV6
@@ -30,14 +31,13 @@ def valid_ip(ip):
     # Check de l'IPv4
     if (re.search(p, ip)):
         return ("Valid IPv4")
-
     # Check de l'IPv6
     elif (re.search(p1, ip)):
         return ("Valid IPv6")
-
     # Retour invalide
     return ("Invalid IP")
 
+# Scan les ports ouverts d'un serveur distant (IPv6 et IPv4)
 def scan_ports(target, start_port, end_port):
     # On vérifie la validité de l'IP
     v_ip = valid_ip(target)
